7764ec654fdee01484dd4c35107249720a904c13
felixx0319 <144153290+felixx0319@users.noreply.github.com>
2025-03-03 17:36:12 +0800

    Update SPMS.c

--- SPMS.c
+++ SPMS.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
+#include <stdbool.h>
 
+void choosing(){
+    printf("Please enter booking: \n");
+    char answer[50];
+    scanf("%s",answer); //scan user input into the answer
+    //fuck trash project who do then who is dog
+    
+}
+
+int main(){
+    bool exit = false;
+    printf("~~ WELCOME TO PLOYU ~~\n");
+    
+    choosing();
+
+}
