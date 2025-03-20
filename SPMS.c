1e5605412223d65a52afc38374c1e15693f48aa1
felix <felix0319z@gmail.com>
2025-03-20 23:09:49 +0800

    fuckyou

--- SPMS.c
+++ SPMS.c
@@ -1,34 +1,136 @@
 #include <stdio.h>
 #include <stdbool.h>   // what is the use of this lib boolean
 #include <stdlib.h>
-#include <unistd.h>
+//#include <unistd.h>
 #include <string.h>
+char* validInput(char command[]);
+void readKeyboard();
+char memory[100][50];
+char buffer[50];
+void addParking(char buffer[]);
 
 void readCommand(){
-    int i =0;
+    int i = 0;
     printf("Please enter booking: \n");
-    char command[50][50];
-    //scan user input into the input
-    while (scanf("%s", command[i]) != EOF) {
-        if (strcmp(command[i]), ";" == 0) {
-            findOutCommand(command[i]);
-            i++;
+    int pid;
+
+    do {
+        readKeyboard();
+        if (strstr(command, "addParking") != NULL) {
+            addParking(char[buffer]);
         }
-    }  
+
+
+
+        else if (strstr(command, "addReservation") != NULL) {}
+        else if (strstr(command, "addEvent" != NULL))  {}
+        else if (strstr(command, "bookEssentials" != NULL)) {}  
+        else if (strstr(command, "printBookings" != NULL)) {}
+    } while (validInput(buffer) != -1);
+
+
+
+
+
+
 }
 
-// The syntax of command is assumed that must correct 
-void findOutCommand(char command[]){
-    if (strstr(command, "addParking") != NULL) {}
-    else if (strstr(command, "addReservation") != NULL) {}
-    else if (strstr(command, "addEvent" != NULL) {}
-    else if (strstr(command, "bookEssentials" != NULL) {}
-    else if (strstr(command, "printBookings" != NULL) {}
-    else if (strstr(command, "endProgram" != NULL) {}
+
+
+//read keyboard
+void readKeyboard(){
+    int bytesread;
+    if ((bytesread = read(0, buffer, sizeof(buffer)-1)) == -1){ //read the user input from keyboard
+        perror("read");
+        exit(1);
+    }
+    buffer[bytesread] = '\0';
 }
+
+
+
+
+int validCommand(char command[]){
+    if (strstr(command, "addParking") != NULL) return 1;
+    else if (strstr(command, "addReservation") != NULL) return 2;
+    else if (strstr(command, "addEvent" != NULL))  return 3;
+    else if (strstr(command, "bookEssentials" != NULL))  return 4;
+    else if (strstr(command, "printBookings" != NULL))  return 5;
+    else if (strstr(command, "endProgram" != NULL))  return 6;
+    else return -1;
+}
+
+
+
+
 
 int main(){
-    bool exit = false;
+    int returnpid;
+    int childpid[4]; //4 module: Input, Scheduling, Output, Analysis
+    int childID;
+    int fd[4][2]; // array of pipeline for parent to children
+    int pfd[4][2]; //array of pipeline for children to parent
+    int i;
+    int j;
+     //create pipeline
+     for (i = 0;i < 4; i++){
+        if (pipe(fd[i]) < 0) { //parent to children
+                printf("Pipeline creation failed\n");
+                exit(1);
+        }
+        if (pipe(pfd[i]) < 0) { //children to parent
+                printf("Pipeline creation failed\n");
+                exit(1);
+        }
+    }
+
+     //create child
+     for (i = 0; i < 4;i++){
+        if ((returnpid = fork()) < 0) {
+                printf("Fork failed\n");
+                exit(1);
+        }
+        if (returnpid == 0){   //child process
+                for(j = 0; j<4; j++){
+                        if (j != i){ //close the unwanted pipeline
+                                close(fd[j][0]);
+                                close(fd[j][1]);
+                                close(pfd[j][0]);
+                                close(pfd[j][1]);
+                        }
+                }
+                close(fd[i][1]); //child close the sender side
+                close(pfd[i][0]); //child close the receiever side
+                childID = i;
+                break;
+        }
+        else{ //parent process
+                close(fd[i][0]);
+                close(pfd[i][1]);
+                childpid[i] = returnpid;
+        }
+     }
+
+
+
+
+
+
+
+
+    if (returnpid == 0){ //child
+        
+    }
+    else{ //parent
+        
+    }
+
+
+
+
+
+
+
     printf("~~ WELCOME TO POLYU ~~\n");
     readCommand();
 
